80fe8defc16d87b96921f7bd4fca6339a2a74b6c
TomSun <tftomsun@web.de>
2017-11-01 01:11:46 +0100

    added test project

---
+++ TomSun.AspNetCore.Extensions.Tests/ExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TomSun.AspNetCore.Extensions.Tests
+{
+    [TestFixture]
+    public class ExtensionsTests
+    {
+        [Test]
+        public void DummyTest()
+        {
+            
+        }
+    }
+}
